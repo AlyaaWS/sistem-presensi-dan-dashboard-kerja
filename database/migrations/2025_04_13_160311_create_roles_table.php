7da4d8d35f1a19d98a7fdbe14b8d49068aacb0a6
AlyaaWS <alyawahyuning93@gmail.com>
2025-04-26 00:47:50 +0700

    mengubah kolom id_role

--- database/migrations/2025_04_13_160311_create_roles_table.php
+++ database/migrations/2025_04_13_160311_create_roles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id('id_role');
+            $table->bigIncrements('id_role');
             $table->string('nama_role');
         });
     }
