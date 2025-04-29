c240e9a0ae6dfc1bf64da820fc4c8e989f89094e
AlyaaWS <alyawahyuning93@gmail.com>
2025-04-30 01:17:40 +0700

    membuat fungsi index

--- app/Http/Controllers/KelolaPenggunaController.php
+++ app/Http/Controllers/KelolaPenggunaController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+
 
 class KelolaPenggunaController extends Controller
 {
-    public function index()
+        public function index()
     {
-        return view('kelolaPengguna');
+        $users = User::with('role')->get();
+
+        return view('kelolaPengguna', compact('users'));
     }
+    public function hapus($id)
+    {
+        $users = User::findOrFail($id);
+        $users->delete();
+
+        return redirect()->route('kelola.pengguna')->with('success', 'Pengguna berhasil dihapus.');
+    }
+
 }
