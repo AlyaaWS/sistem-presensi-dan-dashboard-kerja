a0f56cd215dfcf40517968983feb6d88c76188e7
AlyaaWS <alyawahyuning93@gmail.com>
2025-05-01 00:49:56 +0700

    membuat batasan views user

--- app/Http/Controllers/KelolaPenggunaController.php
+++ app/Http/Controllers/KelolaPenggunaController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Role;
 
 
 class KelolaPenggunaController extends Controller
 {
         public function index()
     {
-        $users = User::with('role')->get();
-
+        $users = User::with('role')
+                 ->whereHas('role', function ($query) {
+                     $query->where('nama_role', 'not like', '%admin%');
+                 })
+                 ->get();
         return view('kelolaPengguna', compact('users'));
     }
     public function hapus($id)
