c2d2c2b8355f5c337433e4f3acf4de1745d6d787
AlyaaWS <alyawahyuning93@gmail.com>
2025-05-05 01:24:11 +0700

    membuat logic tampil halaman kelola pengguna

--- app/Http/Controllers/KelolaPresensiController.php
+++ app/Http/Controllers/KelolaPresensiController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Presensi;
 use Illuminate\Http\Request;
 
 class KelolaPresensiController extends Controller
 {
     public function index()
     {
-        return view('kelolaPresensi');
+        $presensis = Presensi::with('user.role')->get(); // Relasi: presensi → user → role
+        return view('kelolaPresensi', compact('presensis'));
+    }
+
+    public function hapus($id)
+    {
+        $presensi = Presensi::findOrFail($id);
+        $presensi->delete();
+
+        return redirect()->route('kelola.presensi')->with('success', 'Presensi berhasil dihapus!');
     }
 }
