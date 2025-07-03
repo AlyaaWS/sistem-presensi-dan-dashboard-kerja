cd3b3d4a16f973c4b45b6d423ce3f1b06454b8ef
AlyaaWS <alyawahyuning93@gmail.com>
2025-07-03 20:38:57 +0700

    benerin index.php

--- public/index.php
+++ public/index.php
@@ -6,15 +6,14 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Cek mode maintenance
-if (file_exists($maintenance = __DIR__.'/../sistem_presensi_dan_dashboard_kerja/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Composer autoload
-require __DIR__.'/../sistem_presensi_dan_dashboard_kerja/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel dan handle request
-/** @var Application $app */
-$app = require_once __DIR__.'/../sistem_presensi_dan_dashboard_kerja/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
